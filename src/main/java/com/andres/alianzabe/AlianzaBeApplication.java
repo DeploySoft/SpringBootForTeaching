f9f5aa27e6643e3d669642c5f75c639a1ef14922
Pruebas <andres.boyaca@idevelopmentpro.com>
2019-05-21 19:01:11 -0500

    Commit initial

---
+++ src/main/java/com/andres/alianzabe/AlianzaBeApplication.java
@@ -0,0 +1,13 @@
+package com.andres.alianzabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlianzaBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlianzaBeApplication.class, args);
+	}
+
+}
